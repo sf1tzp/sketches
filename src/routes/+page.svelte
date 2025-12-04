42578532ac15f219c612be01e8df71429923e65b
Steven Fitzpatrick <steven@fitz.gg>
2025-12-04 06:52:24 +0000

    Add social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import MosaicMorph from "$lib/components/sketches/mosaic-morph/mosaic-morph.svelte";
     import * as Carousel from "$lib/components/ui/carousel/index.js";
+
+    import Github from '@lucide/svelte/icons/github';
+    import Instagram from '@lucide/svelte/icons/instagram';
 </script>
 
 
@@ -18,4 +21,14 @@
 </Carousel.Root>
 </div>
 
-<h1 class="text-4xl text-center font-semibold text-muted-foreground py-2">Sketches</h1>
+<div class="flex justify-around sm:justify-between align-baseline pb-4 max-w-sm sm:max-w-lg mx-4 sm:mx-auto items-baseline">
+<h1 class="text-4xl text-center font-semibold text-muted-foreground py-4 font-mono">art by fitz</h1>
+<div class="flex gap-4">
+  <a href="https://github.com/sf1tzp">
+    <Github class="size-8 text-muted-foreground"/>
+  </a>
+  <a href="https://instagram.com/sf1tzp">
+    <Instagram class="size-8 text-muted-foreground"/>
+  </a>
+</div>
+</div>
